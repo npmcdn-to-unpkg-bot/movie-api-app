81741867e58a51f03f10af6db3f93a6b48b3946f
Willyb15 <william.steen.bryant@gmail.com>
2016-03-23 17:01:45 -0400

    trying to add typeahead

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+	
 	var imagePath;
 	//The URL of all our API calls
 	var baseURL = 'https://api.themoviedb.org/3/';
@@ -7,6 +8,9 @@ $(document).ready(function(){
 	var apiKey = '?api_key=5e2170b9fb801a61d6d784870c4c2eb1';
 	//The configURL so that we can get basic config data
 	var configURL = baseURL + 'configuration' + apiKey;
+
+
+	
 
 	//Make an AJAX call to the config URL.
 	$.getJSON(configURL, function(configData){
@@ -17,7 +21,6 @@ $(document).ready(function(){
 	//Now Playing is default on page load. Set up the URL
 	var nowPlaying = baseURL + 'movie/now_playing' + apiKey;
 	console.log(nowPlaying);
-	console.log(nowPlaying.results.original_title);
 
 	//Make an AJAX call to the now playing URL.
 
@@ -29,10 +32,54 @@ $(document).ready(function(){
 			newHTML += '<div class="col-sm-3">';
 			newHTML += '<img src="' + currentPoster + '">';
 			newHTML += '</div>';
-			console.log(currentPoster);
+			// console.log(currentPoster);	
 		}
+
+		for(i=0; i<movieData.results.length; i++){
+			var titleArray = [];
+			titleArray.push(movieData.results[i].title);
+			console.log(titleArray);
+		}
+		
+		var substringMatcher = function(strs) {
+		  return function findMatches(q, cb) {
+		    var matches, substringRegex;
+		    matches = [];
+		    substrRegex = new RegExp(q, 'i');
+
+		    $.each(strs, function(i, str) {
+		      if (substrRegex.test(str)) {
+		       		matches.push(str);
+		     	}
+		    });
+		    cb(matches);
+		  };
+		};
+
+		// $('#the-basics .typeahead').typeahead({
+		//   hint: true,
+		//   highlight: true,
+		//   minLength: 1
+		// },
+		// {
+		//   name: 'states',
+		//   source: substringMatcher(titleArray)
+		// });
+
+
 		$('#poster-grid').html(newHTML);
 	});
 
 
+	$('#search').submit(function(){
+		var movieSearchTerm = $('#movie-input').val();
+		console.log(movieSearchTerm);
+		$.getJSON(searchURL, function(movieData){
+			var searchURL = baseURL + '/search/movie'+ apiKey + '?' + movieSearchTerm;
+			console.log(movieData.results[0]);
+		});
+	$('#poster-grid').html(newHTML);
+	});
+
 });
+
