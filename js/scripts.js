256e631a4cca44cc61ecd88791cabbe78275cca3
Willyb15 <william.steen.bryant@gmail.com>
2016-03-24 15:32:23 -0400

    Init commit
    init commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+	$('#searchFilter').change(function(){
+		console.log($(this).val());
+	})
+
 	var imagePath;
 	//The URL of all our API calls
 	var baseURL = 'https://api.themoviedb.org/3/';
@@ -6,6 +11,7 @@ $(document).ready(function(){
 	var apiKey = '?api_key=5e2170b9fb801a61d6d784870c4c2eb1';
 	//The configURL so that we can get basic config data
 	var configURL = baseURL + 'configuration' + apiKey;
+	genreArray = [];
 
 	//Make an AJAX call to the config URL.
 	$.getJSON(configURL, function(configData){
@@ -13,20 +19,52 @@ $(document).ready(function(){
 		imagePath = configData.images.base_url;
 	});
 
+
+	var genreURL = baseURL + 'genre/movie/list' + apiKey;
+	//Make an AJAX call to the config URL.
+	$.getJSON(genreURL, function(movieGenre){
+		//Set our global var imagePath to the result of our AJAX call
+			console.log(movieGenre);
+		for(i=0; i<movieGenre.genres.length; i++){
+			var genreName = movieGenre.genres[i].name;
+			var genreId = movieGenre.genres[i].id;
+			console.log(genreName);
+			console.log(genreId);
+			// genreArray.push(genreId);
+			genreArray[genreId] = movieGenre.genres[i].name;
+			console.log(genreArray);
+		}
+	});
+
+
 	//Now Playing is default on page load. Set up the URL
 	var nowPlaying = baseURL + 'movie/now_playing' + apiKey;
 	//Make an AJAX call to the now playing URL.
 	$.getJSON(nowPlaying, function(movieData){
+		console.log(movieData);
 		var newHTML = '';
 		//Loop through all the results and set up an image url.
+
+
 		for(i=0; i<movieData.results.length; i++){
 			var currentPoster = imagePath + 'w300' + movieData.results[i].poster_path;
-			newHTML += '<div class="col-sm-3">';
+			
+			for(j=0; j<movieData.results.length; j++){
+				var genreNum = movieData.results[j].genre_ids;
+				console.log(genreNum);
+			
+		
+			newHTML += '<div class="col-sm-3 now-playing ' + genreNum + '">';
 			newHTML += '<img src="' + currentPoster + '">';
 			newHTML += '</div>';
 			// console.log(currentPoster);
 		}
+		}
+
+
 		$('#poster-grid').html(newHTML);
+
+		getIsotope();
 	});
 
 
@@ -38,7 +76,7 @@ $(document).ready(function(){
 		var userSearch = $('#searchText').val();
 		//Filter the user searched for (movie, actor, etc.)
 		var searchFilter = $('#searchFilter').val();
-		console.log(searchFilter);
+		// console.log(searchFilter);
 
 		//Setup the endpoing to use the value of the select box as the parameter after /search
 		var searchURL = baseURL + 'search/' + searchFilter + apiKey + '&query=' + encodeURI(userSearch);
@@ -48,7 +86,6 @@ $(document).ready(function(){
 		// -- FOR WHEN WE WERE ONLY SEARCHING FOR MOVIES - var movieSarch = baseURL + 'search/movie' + apiKey + '&query=' + encodeURI(userSearch);
 		//Make an AJAX call to the now playing URL.q
 		$.getJSON(searchURL, function(movieData){
-			console.log(movieData);
 			var newHTML = '';
 			//Loop through all the results and set up an image url.
 			for(i=0; i<movieData.results.length; i++){
@@ -58,6 +95,9 @@ $(document).ready(function(){
 				}else{
 					var currentPoster = imagePath + 'w300' + movieData.results[i].poster_path;
 				}
+
+				// console.log(movieData.results[i]);
+
 				newHTML += '<div class="col-sm-3">';
 				newHTML += '<img src="' + currentPoster + '">';
 				newHTML += '</div>';
@@ -66,6 +106,14 @@ $(document).ready(function(){
 			$('#poster-grid').html(newHTML);
 		});		
 		event.preventDefault();
+	});
+
+	//isotope listener
+	$('#comedy-filter').click(function(){
+		$('#poster-grid').isotope({ filter: '.comedy' })
+
+
+
 	});
 
 });
@@ -96,18 +144,52 @@ var substringMatcher = function(strs) {
   };
 };
 
+
+var arrayToSearch = [];
+for (i=1; i <= 6; i++) {
+	var popularMovies = 'https://api.themoviedb.org/3/movie/popular?api_key=fec8b5ab27b292a68294261bb21b04a5&page=' + i;
+	// console.log(popularMovies);
+	$.getJSON(popularMovies, function(popularM){
+		for(j=0; j<popularM.results.length; j++){
+			arrayToSearch.push(popularM.results[j].original_title);
+		}
+		// console.log(arrayToSearch);
+	});
+}
+
+
+
+
+
 var actors = [
 	'Brad Pitt',
 	'Michael Douglas',
 	'Al Pacino'
 ];
 
-$('#movie-form .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'actors',
-  source: substringMatcher(actors)
-});
+$('#movie-form .typeahead').typeahead(
+	{
+  		hint: true,
+  		highlight: true,
+  		minLength: 3
+	},
+	{
+  		name: 'actors',
+  		source: substringMatcher(arrayToSearch)
+	}
+);
+
+function getIsotope(){
+	$('#poster-grid').isotope(
+		{
+	  		// options
+	  		itemSelector: '.now-playing',
+	  		layoutMode: 'fitRows'
+	  	}
+	);			
+}
+
+
+
+
+
